eefb3449eb20d68eef470bf62453918f8422cb6a
nKKai <46084374+nKiux@users.noreply.github.com>
2024-06-07 22:21:55 +0800

    version 2

--- stt_jpn.py
+++ stt_jpn.py
@@ -1,41 +1,70 @@
 import os
-try:
-    import speech_recognition as sr
-    import translate
-    import asyncio
-except:
-    os.system("pip install speechrecognition")
-    os.system("pip install translate")
+
+from threading import Thread
+from queue import Queue
+
+import speech_recognition as sr
+import asyncio
 
 # Initialize the recognizer
 r = sr.Recognizer()
-r.energy_threshold = 350
+r.energy_threshold = 280
 r.dynamic_energy_ratio = 2
 
-async def detecting():
+global audio_q
+audio_q = Queue()
+
+def collect():
     #print("__偵測: [啟動中...]")
     try:
-        with sr.Microphone(device_index=2) as source2:
-            #print("__偵測: [聆聽中...]")
-            r.adjust_for_ambient_noise(source2, duration = 0.5)
-            audio2 = r.listen(source2, phrase_time_limit = 15, timeout=1)
-            print('understanding...')
-            MyText = r.recognize_google(audio2, language='ja-JP')
-            print("Detector:", MyText)
-            await open("srcipt.txt", "a", encoding="utf-8").write(f"{MyText} \n")
-            
-    except sr.RequestError as e:
-        #print("__偵測: [無法處理]; {0}".format(e))
-        pass
-    except:
-        #print("Skip...", flush=True)
-        pass
+        while True:
+            with sr.Microphone(device_index=2) as source:
+                r.adjust_for_ambient_noise(source, duration = 0.5)
+                audio_q.put(r.listen(source, phrase_time_limit = 15))
+                print('saved...')
+    except KeyboardInterrupt:
+        print('Quit')
+        exit()
 
-async def main():
-    await detecting()
+    except sr.exceptions.WaitTimeoutError:
+        print('skipped a timeout')
+
+def recognizer():
+    while True:
+        audio = audio_q.get()
+        if audio is None: 
+            print('None')
+            break
+        try:
+            #r.adjust_for_ambient_noise(audio, duration = 0.5)
+            #audio = r.listen(audio, phrase_time_limit = 15, timeout=1)
+            #print('understanding...')
+            MyText = r.recognize_google(audio, language='ja-JP')
+            print("Detector:", MyText)
+            open("srcipt.txt", "a", encoding="utf-8").write(f"{MyText} \n")
+        except sr.RequestError as e:
+            print("**處理錯誤**; {0}".format(e))
+
+        except sr.UnknownValueError:
+            print("**無法識別**")
+        print('done...')
+        audio_q.task_done()
+
+def main():
+    global rec_thread
+    rec_thread = Thread(target=recognizer)
+    rec_thread.daemon = True
+    rec_thread.start()
+    #print('collecting...')
+    collect()
+    #print('exit collect')
+    audio_q.join()
+    audio_q.put(None)
+    rec_thread.join()
+    print('finished')
 
 wrt = open("srcipt.txt", "w", encoding="utf-8")
 wrt.write("")
 
-while True:
-    asyncio.run(main())
+
+main()
--- trans_jpn_tw.py
+++ trans_jpn_tw.py
@@ -1,12 +1,28 @@
 #import translate
-from deep_translator import GoogleTranslator
+from deep_translator import GoogleTranslator, DeeplTranslator
 queue = []
 
-translator = GoogleTranslator(source='ja', target='zh-TW')
+open(".env", "a", encoding="utf-8").close()
+KeyRead = open(".env", "r", encoding="utf-8").read().split('\n')
+Key = KeyRead[0]
+
+TestPassed = True
+
+if "#" not in Key:
+    print(f"DeepL API Key: ", Key)
+    try:
+        translator = DeeplTranslator(api_key=Key, source="ja", target="zh", use_free_api=True)
+        translator.translate("Test")
+    except:
+        TestPassed = False
+        print('DeepL模式：API Key不正確或者不可使用，請檢查\n若您是DeepL付費用戶，請將use_free_api更改為False\n若不使用DeepL，請將.env內容修改為井字號(#)')
+else:
+    translator = GoogleTranslator(source='ja', target='zh-TW')
 
 lasttrans = "none"
 
-while(True):
+if TestPassed:
+    while True:
         queue = open("srcipt.txt", "r", encoding="utf-8").read().split('\n')
         if len(queue) > 1:
             trans = queue[-2]
@@ -14,3 +30,9 @@ while(True):
                 trans = translator.translate(queue[-2])
                 print(trans)
                 lasttrans = queue[-2]
+else:
+    print('\n發生錯誤，待機中...\n請重新啟動程式')
+    while(True):
+        pass
+
+     
